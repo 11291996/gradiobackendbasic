a3ce4f0edc634eed9ba4b3c70de63a06d09f3d1e
11291996 <jaewan1040@yonsei.ac.kr>
2023-12-21 15:01:14 +0900

    12212023

---
+++ interface_state.py
@@ -0,0 +1,71 @@
+import gradio as gr
+
+#global state for the interface
+#saves data for entire launch
+scores = [] #this is outside of the interface, so global, which means every user of this launch will have access to this data
+
+def track_score(score):
+    scores.append(score)
+    top_scores = sorted(scores, reverse=True)[:3]
+    return top_scores
+
+demo1 = gr.Interface(
+    track_score, 
+    gr.Number(label="Score"), 
+    gr.JSON(label="Top Scores") #displays json format clearer 
+)
+
+demo1.launch()
+
+#session state for the interface
+#save data for each users
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
+model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+
+
+def user(message, history):
+    return "", history + [[message, None]]
+
+
+def bot(history):
+    user_message = history[-1][0]
+    new_user_input_ids = tokenizer.encode(
+        user_message + tokenizer.eos_token, return_tensors="pt"
+    )
+
+    #append the new user input tokens to the chat history
+    bot_input_ids = torch.cat([torch.LongTensor([]), new_user_input_ids], dim=-1)
+
+    #generate a response
+    response = model.generate(
+        bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id
+    ).tolist()
+
+    # convert the tokens to text, and then split the responses into lines
+    response = tokenizer.decode(response[0]).split("<|endoftext|>")
+    response = [
+        (response[i], response[i + 1]) for i in range(0, len(response) - 1, 2)
+    ]  # convert to tuples of list
+    history[-1] = response[0]
+    return history
+
+
+with gr.Blocks() as demo2:
+    chatbot = gr.Chatbot()
+    msg = gr.Textbox()
+    clear = gr.Button("Clear")
+
+    msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(
+        bot, chatbot, chatbot
+    ) #submit uses the function when it is clicked, gets the defined input and returns the defined output
+    #then uses the function continually when it is clicked after the submit button is clicked
+    #queue is the same with interface queue but for gradio objects
+    #submit creates sessions for each user 
+    #then saves the data for each user
+    #this is a model for session state
+    clear.click(lambda: None, None, chatbot, queue=False)
+
+demo2.launch()
